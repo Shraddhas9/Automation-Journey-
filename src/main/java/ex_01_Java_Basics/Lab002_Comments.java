4db4baf033bb094d90645a2aae0d3be2e46d8d63
Shraddha Walunj <shraddhasw99@gmail.com>
2025-12-17 15:20:01 +0530

    17th Dec , Java Basics

---
+++ src/main/java/ex_01_Java_Basics/Lab002_Comments.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basics;
+
+public class Lab002_Comments {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/ex_01_Java_Basics/Lab003_Keywords.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basics;
+
+public class Lab003_Keywords {
+        public static void main(String[] args) {
+            System.out.println("Keywords!");
+        }
+
+    }
+
+
+
+// package, public, class, static, void
