9a0497233596a4c801a30d7cda7c89fbb577bfd0
Shraddha Walunj <shraddhasw99@gmail.com>
2025-12-22 15:54:22 +0530

    19th Dec , 2025 ->Literals completed

---
+++ src/main/java/ex_03_Literals/Lab006_Non_Primitive_Literals.java
@@ -0,0 +1,13 @@
+package ex_03_Literals;
+
+public class Lab006_Non_Primitive_Literals {
+
+        public static void main(String[] args) {
+            String name = "Pramod"; // String is bunch of char.
+            int[] arrays_of_items = new int[10];
+
+            char A = '0';
+            System.out.println(A);
+        }
+    }
+
---
+++ src/main/java/ex_03_Literals/Lab006_String_NullValues.java
@@ -0,0 +1,15 @@
+package ex_03_Literals;
+
+public class Lab006_String_NullValues {
+
+        public static void main(String[] args) {
+            String name = null;
+            String name1 = "";
+            char c = '0';
+            //int a = null;
+
+            System.out.println(name1+"      "+c);
+
+
+        }
+    }
