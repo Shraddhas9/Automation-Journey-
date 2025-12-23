5de3c02f6d1d93965430d53ecca51045b1421ccf
Shraddha Walunj <shraddhasw99@gmail.com>
2025-12-23 10:01:20 +0530

    20th Dec , 2025 ->Operators in Java

---
+++ src/main/java/ex_04_Operators/Lab014_Operators.java
@@ -0,0 +1,17 @@
+package ex_04_Operators;
+
+public class Lab014_Operators {
+
+        public static void main(String[] args) {
+            int age = 10;
+            // Assignment operator
+            // int -> data type
+            // age -> identifier
+            // = -> Assignment operator
+            // 10 -> Literal
+
+            // RHV to the LH
+
+        }
+    }
+
